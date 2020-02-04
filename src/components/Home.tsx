b798bdae764e1ac1f9f4d1b29fe2da4bfa314ca2
Seanhetzel <seanhetzel1@gmail.com>
2020-02-03 22:36:15 -0800

    removed temp button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -26,8 +26,8 @@ export default class Home extends Component<Props, State> {
   render() {
     return (
       <>
-        hi
-        <Button color="success">Success</Button>
+        {/* hi
+        <Button color="success">Success</Button> */}
 
       </>
     )
