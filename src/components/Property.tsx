303602884f9e7b25abba9ac1da2a50b058d6a979
unknown <seanhetzel1@gmail.com>
2020-02-21 16:58:26 -0800

    added temp image to property page

--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "components/NavBar";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Col, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import tempHouse1 from "../assets/img/tempHouse1.jpg"
 
 interface Props {}
 interface State {}
@@ -16,6 +17,14 @@ export default class Property extends Component<Props, State> {
           <BreadcrumbItem>Home</BreadcrumbItem>
           <BreadcrumbItem>Property</BreadcrumbItem>
         </Breadcrumb>
+        <Col lg="4" md="5">
+          <img
+            alt="..."
+            className="img-fluid"
+            src={require("assets/img/tempHouse1.jpg")}
+          />
+        </Col>
+        <h1 className="primary">$Money</h1>
       </>
     );
   }
